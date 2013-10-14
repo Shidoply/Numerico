40538315aa3bf5937266bdbc7c90fc25c498d9c9
Gonzalo Ávila Alterach <gonzaloavilaalterach@gmail.com>
2013-10-14 16:12:54 -0300

    Rearmé los gráficos con repetición

--- numerico/salida/graficar.m
+++ numerico/salida/graficar.m
@@ -5,6 +5,6 @@ clear all;
 graphics_toolkit("gnuplot");
 
 figure;
-datos = dlmread("dia4.csv","\t",1,0);
+datos = dlmread("dia4.csv","\t",0,0);
 polar ((360-datos(:,1)+90)*pi/180, datos(:,2));
 print('dia4.png','-dpng');
